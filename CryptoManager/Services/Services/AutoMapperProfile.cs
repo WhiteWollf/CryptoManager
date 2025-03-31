851a9d27d9f75c0948c080be4ca62d23a6aaaecd
WhiteWolf <khaszon@gmail.com>
2025-03-31 10:18:44 +0200

    AutoMapperProfile organization

--- CryptoManager/Services/Services/AutoMapperProfile.cs
+++ CryptoManager/Services/Services/AutoMapperProfile.cs
@@ -11,30 +11,29 @@ namespace Services.Services
             CreateMap<User, UserDto>()
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Name))
                 .ReverseMap();
+
             CreateMap<UserRegisterDto, User>();
+
             CreateMap<UserUpdateDto, User>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Username));
-
-            CreateMap<Role, RoleDto>().ReverseMap();
 
             CreateMap<UserRegisterDto, User>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Username))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
 
+            //Role Mappings
+            CreateMap<Role, RoleDto>().ReverseMap();
+
+            //WalletMappings
             CreateMap<Wallet, WalletDto>().ReverseMap();
 
-            CreateMap<Crypto, CryptoDto>().ReverseMap();
-
             CreateMap<Wallet, WalletDetailDto>()
-            .ForMember(dest => dest.Cryptos, opt => opt.MapFrom(src => src.WalletCryptos));
-            CreateMap<WalletCrypto, WalletCryptoDetailDto>()
-                .ForMember(dest => dest.CryptoName, opt => opt.MapFrom(src => src.Crypto.Name))
-                .ForMember(dest => dest.Symbol, opt => opt.MapFrom(src => src.Crypto.Symbol))
-                .ForMember(dest => dest.BuyPrice, opt => opt.MapFrom(src => src.BuyPrice));
-
+                .ForMember(dest => dest.Cryptos, opt => opt.MapFrom(src => src.WalletCryptos));
             CreateMap<Wallet, PortfolioDto>()
-           .ForMember(dest => dest.Cryptos, opt => opt.MapFrom(src => src.WalletCryptos))
-           .ForMember(dest => dest.TotalValue, opt => opt.MapFrom(src => src.WalletCryptos.Sum(wc=>wc.Amount*wc.Crypto.Price)));
+
+               .ForMember(dest => dest.Cryptos, opt => opt.MapFrom(src => src.WalletCryptos))
+
+               .ForMember(dest => dest.TotalValue, opt => opt.MapFrom(src => src.WalletCryptos.Sum(wc=>wc.Amount*wc.Crypto.Price)));
             CreateMap<WalletCrypto, PortfolioDetailDto>()
                 .ForMember(dest => dest.CryptoName, opt => opt.MapFrom(src => src.Crypto.Name))
                 .ForMember(dest => dest.Symbol, opt => opt.MapFrom(src => src.Crypto.Symbol))
@@ -42,10 +41,21 @@ namespace Services.Services
                 .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount))
                 .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Amount * src.Crypto.Price));
 
+            //WalletCrypto Mappings
+            CreateMap<WalletCrypto, WalletCryptoDetailDto>()
+                .ForMember(dest => dest.CryptoName, opt => opt.MapFrom(src => src.Crypto.Name))
+                .ForMember(dest => dest.Symbol, opt => opt.MapFrom(src => src.Crypto.Symbol))
+                .ForMember(dest => dest.BuyPrice, opt => opt.MapFrom(src => src.BuyPrice));
+
+            //Crypto Mappings
+            CreateMap<Crypto, CryptoDto>().ReverseMap();
+
+            //CryptoPriceLog Mappings
             CreateMap<CryptoPriceLog, CryptoPriceLogDto>()
                 .ForMember(dest => dest.CryptoName, opt => opt.MapFrom(src => src.Crypto.Name))
                 .ForMember(dest => dest.Symbol, opt => opt.MapFrom(src => src.Crypto.Symbol));
 
+            //TransactionLog Mapping
             CreateMap<TransactionLog, TransactionDto>()
                 .ForMember(dest => dest.CryptoName, opt => opt.MapFrom(src => src.Crypto.Name))
                 .ForMember(dest => dest.Symbol, opt => opt.MapFrom(src => src.Crypto.Symbol))
